df7e6e8acd7525f6262554b4e38b11a075d54659
jbolanyos <jbolanyos@gmail.com>
2024-12-22 15:27:29 -0600

    actualización 22 de diciembre 15:27

--- pages/1_⚓_EDA.py
+++ pages/1_⚓_EDA.py
@@ -6,7 +6,12 @@ import pandas as pd
 def load_data():
     return pd.read_csv("data/StudentPerformanceFactors.csv")
 
-df = load_data()
+#df = load_data()
+
+#Cargamos los datos del archivo csv:
+url='https://drive.google.com/file/d/1TS8BpC3CBTlEHlCSlHfIDlbgSE4jDL5q/view?usp=sharing'
+url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+df = pd.read_csv(url)
 
 st.header("ANALISIS EXPLORATORIO")
 
@@ -14,3 +19,5 @@ with st.container():
     col1, col2, col3 = st.columns(3)
     with col1:
         st.metric(label="Número de filas:", value=df.shape[0], border=True)
+
+
