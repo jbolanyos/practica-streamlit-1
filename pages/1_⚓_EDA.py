9ef5b842469f67d7d00f6000a2851cef09389ad6
jbolanyos <jbolanyos@gmail.com>
2024-12-22 15:56:59 -0600

    actualización 22 de diciembre 15:56

--- pages/1_⚓_EDA.py
+++ pages/1_⚓_EDA.py
@@ -2,16 +2,16 @@ import streamlit as st
 import pandas as pd
 
 #Cargar el DataFrame
-#@st.cache_data
-#def load_data():
-#    return pd.read_csv("data/StudentPerformanceFactors.csv")
+@st.cache_data
+def load_data():
+    return pd.read_csv("data/StudentPerformanceFactors.csv")
 
-#df = load_data()
+df = load_data()
 
 #Cargamos los datos del archivo csv:
-url='https://drive.google.com/file/d/1TS8BpC3CBTlEHlCSlHfIDlbgSE4jDL5q/view?usp=sharing'
-url='https://drive.google.com/uc?id=' + url.split('/')[-2]
-df = pd.read_csv(url)
+#url='https://drive.google.com/file/d/1TS8BpC3CBTlEHlCSlHfIDlbgSE4jDL5q/view?usp=sharing'
+#url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+#df = pd.read_csv(url)
 
 st.header("ANALISIS EXPLORATORIO")
 
