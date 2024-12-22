916afef1b9536b724cd14cd0cf1596ff67d4462b
jbolanyos <jbolanyos@gmail.com>
2024-12-22 07:54:45 -0600

    actualización 22 de diciembre 7:54

---
+++ Inicio.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import os
+
+st.set_page_config(layout="wide")
+
+st.title("Proyecto Final")
+
+col1, col2 = st.columns([2,2])
+
+with col1:
+    st.subheader("Proyecto Final")
+    st.subheader("FIFA Statistics")
