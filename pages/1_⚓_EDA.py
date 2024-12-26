d95c9d0f5c6cb72e6a7628b8b971db36a80ea521
jbolanyos <jbolanyos@gmail.com>
2024-12-25 18:41:56 -0600

    actualización 25 de diciembre 18:41

--- pages/1_⚓_EDA.py
+++ pages/1_⚓_EDA.py
@@ -2,20 +2,22 @@ import streamlit as st
 import pandas as pd
 
 #Cargar el DataFrame
-@st.cache_data
-def load_data():
-    return pd.read_csv("data/StudentPerformanceFactors.csv")
+#@st.cache_data
+#def load_data():
+#    return pd.read_csv("data/StudentPerformanceFactors.csv")
 
-df = load_data()
+#df = load_data()
 
-st.image("utils/1241.jpg")
+st.header("PROYECTO FIFA - ANÁLISIS EXPLORATORIO DE DATOS")
+
+#st.image("utils/1241.jpg")
 
 #Cargamos los datos del archivo csv:
-#url='https://drive.google.com/file/d/1TS8BpC3CBTlEHlCSlHfIDlbgSE4jDL5q/view?usp=sharing'
-#url='https://drive.google.com/uc?id=' + url.split('/')[-2]
-#df = pd.read_csv(url)
+url='https://drive.google.com/file/d/1TS8BpC3CBTlEHlCSlHfIDlbgSE4jDL5q/view?usp=sharing'
+url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+df = pd.read_csv(url)
 
-st.header("ANALISIS EXPLORATORIO")
+#st.header("ANALISIS EXPLORATORIO")
 
 with st.container():
     col1, col2, col3 = st.columns(3)
